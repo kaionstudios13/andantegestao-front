ccd77c1c09fa7cc04f5fe2b164640a92f29c3903
Samuel Veloso <k4ionstudios@gmail.com>
2025-12-12 10:06:00 -0300

    socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,15 +8,29 @@ const Footer: React.FC = () => {
           <h3 className="text-white font-serif font-bold text-lg">Andante</h3>
           <p className="text-sm mt-1">Gestão de Cultura</p>
         </div>
-        
+
         <div className="text-sm font-light">
-          &copy; {new Date().getFullYear()} Marcelo Bones. Todos os direitos reservados. 
+          &copy; {new Date().getFullYear()} Marcelo Bones. Todos os direitos reservados.
           <p>Produzido por Sam Veloso - Kaion Studios.</p>
         </div>
-        
+
         <div className="mt-4 md:mt-0 flex space-x-6 text-sm">
-          <a href="#" className="hover:text-white transition-colors">Instagram</a>
-          <a href="#" className="hover:text-white transition-colors">LinkedIn</a>
+          <a
+            href="https://www.instagram.com/marcelobones/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition-colors"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.linkedin.com/in/marcelo-bones-8456832a/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition-colors"
+          >
+            LinkedIn
+          </a>
         </div>
       </div>
     </footer>
